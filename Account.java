b39cda2e5cf52e531e28d35aa9b749bd45660cc9
TintiGust <45256578+TintiGust@users.noreply.github.com>
2021-11-30 23:30:19 +0100

    Update Account.java

--- Account.java
+++ Account.java
@@ -25,8 +25,14 @@ public class Account {
     }
     public void transfer (float transferAmount, Account destinataire){
         withdraw(transferAmount);
-        destinataire.deposit(transferAmount);
+        destinataire.deposit(transferAmount); 
     }
 
 
 }
+/* demande d'explication
+la ligne 26 définit une variable destinataire de type Account ?, donc lorsque un objet Account sera instancié, j'aurais un objet Account dans destinataire ?
+la ligne 28 apelle la fonction déposit avec parametre transferAmount,
+ainsi, je comprend avec cette syntaxe que j'apelle la procédure deposit pour modifier la var Amount de mon objet destinataire de type Account (comme si j'apellais déposit
+dans mon objet actuel, pour modifier son atribut Amount qui possède sa valeur unique)
+est ce que je vois bien la chose ?
