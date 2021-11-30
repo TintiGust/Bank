5e61a43fdae05b3c0e1f0abba01915bbd2b68e06
TintiGust <45256578+TintiGust@users.noreply.github.com>
2021-11-30 16:47:41 +0100

    Add files via upload

---
+++ Account.java
@@ -0,0 +1,22 @@
+package fr.training.BankingSystem;
+
+public class Account {
+    public int id;
+    public int amount;
+
+    public static void main(String[] args) {
+
+    }
+    public void deposit(){
+
+    }
+    public void withdraw(){
+
+    }
+    public float getAmount(){
+
+    }
+    public void showAmount(){
+
+    }
+}
---
+++ MainBank.java
@@ -0,0 +1,7 @@
+package fr.training.BankingSystem;
+
+public class MainBank {
+    public static void main(String[] args) {
+
+    }
+}
