27271149b52f2e68dc9a15f4251770b59421d016
augustinguillon <aguillon.ent@gmail.com>
2021-12-07 12:12:00 +0100

    update

---
+++ src/bank/BankAccount.java
@@ -0,0 +1,23 @@
+package bank;
+
+public class BankAccount {
+    private int id;
+    private int amount;
+
+    BankAccount(){
+
+    }
+
+    private void deposit (){
+
+    }
+
+    private void withDraw(){
+
+    }
+
+    private void seeAmount(){
+        
+    }
+
+}
